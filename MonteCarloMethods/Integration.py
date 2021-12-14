35b74ea1eba772f5329aef25f9ec9cc28918a9d4
Дмитрий Яковенко <5430392-dimyakov@users.noreply.gitlab.com>
2021-12-14 18:40:33 +0600

    fix(MonteCarloMethods/Integration): added random integration grid

--- MonteCarloMethods/Integration.py
+++ MonteCarloMethods/Integration.py
@@ -9,7 +9,7 @@ class Integration:
         bottom_bounds = list(map(lambda bound: bound[0], bounds))
         top_bounds = list(map(lambda bound: bound[1], bounds))
         n_args = len(bounds)
-        args = np.transpose([ np.linspace(bottom_bounds[i], top_bounds[i], n) for i in range(n_args) ])
+        args = np.transpose([ np.random.uniform(bottom_bounds[i], top_bounds[i], n) for i in range(n_args) ])
         function_mean = reduce(lambda f_sum, args: f_sum + function(args), args, 0) / n
         bound_factor = reduce(lambda factor_prod, i: factor_prod * (top_bounds[i] - bottom_bounds[i]), list(range(n_args)), 1)
         return bound_factor * function_mean
--- MonteCarloMethods/use.py
+++ MonteCarloMethods/use.py
@@ -2,7 +2,7 @@ import functools
 from . import Integration
 
 bounds = [(0, 1), (1, 2), (0, 3)]
-n = 100
+n = 10000
 
 def function(args):
     return 3*args[0] + args[1] + args[2]

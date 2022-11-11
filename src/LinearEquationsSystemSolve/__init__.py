08572b9164b13ff1b864ca222dc0fd1e3f3e6da9
Дмитрий Яковенко <5430392-dimyakov@users.noreply.gitlab.com>
2022-11-11 13:16:36 +0600

    refactor(src/LinearEquationsSystemSolve/): deleted __init__.py

--- src/LinearEquationsSystemSolve/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Float import Float
-from .Gauss import Gauss
-from .GaussWithError import GaussWithError
-from .InverseMatrix import InverseMatrix
-from .SimpleIter_Siedel import SimpleIter_Siedel
-from .SimpleIter_SiedelWithCheck import SimpleIter_SiedelWithConvergenceCheck
-from .Tridiagonal import Tridiagonal

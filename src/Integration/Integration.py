9725a8982fc12ed17ec87d6a234e5f0548ad8854
Дмитрий Яковенко <5430392-dimyakov@users.noreply.gitlab.com>
2022-11-11 13:15:08 +0600

    refactor(src/Integration/): deleted classes and redundant imports

--- src/Integration/Integration.py
+++ src/Integration/Integration.py
@@ -4,68 +4,59 @@ from types import FunctionType
 
 from numpy.core.fromnumeric import mean
 
-class IntegrationMethods(Enum):
-    Left = "left"
-    Right = "right"
-    Central = "center"
-    Trapezoid = "trapezoid"
-    Parabola = "parabola"
+INTEGRATION_METHOD_LEFT = "left"
+INTEGRATION_METHOD_RIGHT = "right"
+INTEGRATION_METHOD_CENTRAL = "center"
+INTEGRATION_METHOD_TRAPEZOID = "trapezoid"
+INTEGRATION_METHOD_PARABOLA = "parabola"
 
-class Integration:
 
-    @staticmethod
-    def calculate(function: FunctionType, start: float, stop: float, intervals: float, method: IntegrationMethods):
-        x = np.linspace(start, stop, intervals + 1)
+def calculate(function: FunctionType, start: float, stop: float, intervals: float, method: str):
+    x = np.linspace(start, stop, intervals + 1)
 
-        if method.value == IntegrationMethods.Parabola.value:
-            x = np.linspace(start, stop, 2*intervals + 1)
+    if method == INTEGRATION_METHOD_PARABOLA:
+        x = np.linspace(start, stop, 2*intervals + 1)
 
-        sum = 0
-        for i in range(intervals):
-            sum += Integration.__calculate_rectangle_quadrature(function, x, i, method)
+    sum = 0
+    for i in range(intervals):
+        sum += __calculate_rectangle_quadrature(function, x, i, method)
 
-        return sum
+    return sum
 
-    @staticmethod
-    def __calculate_rectangle_quadrature(function: FunctionType, x: list, i: int, method: IntegrationMethods):
-        if method.value == IntegrationMethods.Left.value:
-            return Integration.__calculate_left_rectange_quadrature(function, x[i], x[i+1])
+def __calculate_rectangle_quadrature(function: FunctionType, x: list, i: int, method: str):
+    if method == INTEGRATION_METHOD_LEFT:
+        return __calculate_left_rectange_quadrature(function, x[i], x[i+1])
 
-        if method.value == IntegrationMethods.Right.value:
-            return Integration.__calculate_right_rectange_quadrature(function, x[i], x[i+1])
+    if method == INTEGRATION_METHOD_RIGHT:
+        return __calculate_right_rectange_quadrature(function, x[i], x[i+1])
 
-        if method.value == IntegrationMethods.Central.value:
-            return Integration.__calculate_center_rectange_quadrature(function, x[i], x[i+1])
+    if method == INTEGRATION_METHOD_CENTRAL:
+        return __calculate_center_rectange_quadrature(function, x[i], x[i+1])
 
-        if method.value == IntegrationMethods.Trapezoid.value:
-            return Integration.__calculate_trapezoid_quadrature(function, x[i], x[i+1])
+    if method == INTEGRATION_METHOD_TRAPEZOID:
+        return __calculate_trapezoid_quadrature(function, x[i], x[i+1])
 
-        if method.value == IntegrationMethods.Parabola.value:
-            return Integration.__calculate_parabola_quadrature(function, x[2*i], x[2*i + 1], x[2*i + 2])
+    if method == INTEGRATION_METHOD_PARABOLA:
+        return __calculate_parabola_quadrature(function, x[2*i], x[2*i + 1], x[2*i + 2])
 
-    @staticmethod
-    def __calculate_left_rectange_quadrature(function: FunctionType, left_value: float, right_value: float):
-        h = np.abs(right_value - left_value)
-        f = function(left_value)
-        return h * f
+def __calculate_left_rectange_quadrature(function: FunctionType, left_value: float, right_value: float):
+    h = np.abs(right_value - left_value)
+    f = function(left_value)
+    return h * f
 
-    @staticmethod
-    def __calculate_right_rectange_quadrature(function: FunctionType, left_value: float, right_value: float):
-        h = np.abs(right_value - left_value)
-        f = function(right_value)
-        return h * f
+def __calculate_right_rectange_quadrature(function: FunctionType, left_value: float, right_value: float):
+    h = np.abs(right_value - left_value)
+    f = function(right_value)
+    return h * f
 
-    @staticmethod
-    def __calculate_center_rectange_quadrature(function: FunctionType, left_value: float, right_value: float):
-        h = np.abs(right_value - left_value)
-        center = (right_value + left_value) / 2
-        f = function(center)
-        return h * f
+def __calculate_center_rectange_quadrature(function: FunctionType, left_value: float, right_value: float):
+    h = np.abs(right_value - left_value)
+    center = (right_value + left_value) / 2
+    f = function(center)
+    return h * f
 
-    @staticmethod
-    def __calculate_trapezoid_quadrature(function: FunctionType, left_value: float, right_value: float):
-        return (function(left_value) + function(right_value)) * (right_value - left_value) / 2
+def __calculate_trapezoid_quadrature(function: FunctionType, left_value: float, right_value: float):
+    return (function(left_value) + function(right_value)) * (right_value - left_value) / 2
 
-    @staticmethod
-    def __calculate_parabola_quadrature(function: FunctionType, left_value: float, middle_value: float, right_value: float):
-        return (function(left_value) + 4*function(middle_value) + function(right_value)) * (right_value - left_value) / 6
+def __calculate_parabola_quadrature(function: FunctionType, left_value: float, middle_value: float, right_value: float):
+    return (function(left_value) + 4*function(middle_value) + function(right_value)) * (right_value - left_value) / 6
--- src/Integration/use.py
+++ src/Integration/use.py
@@ -1,4 +1,4 @@
-from Integration import Integration, IntegrationMethods
+import Integration
 
 def function(x):
     return 3*x**2 + 5 + x**0.5
@@ -8,17 +8,17 @@ start = 0
 stop = 3
 
 print("Метод левых прямоугольников: {}".format(
-    Integration.calculate(function, start, stop, n, IntegrationMethods.Left)
+    Integration.calculate(function, start, stop, n, Integration.INTEGRATION_METHOD_LEFT)
 ))
 print("Метод правых прямоугольников: {}".format(
-    Integration.calculate(function, start, stop, n, IntegrationMethods.Right)
+    Integration.calculate(function, start, stop, n, Integration.INTEGRATION_METHOD_RIGHT)
 ))
 print("Метод центральных прямоугольников: {}".format(
-    Integration.calculate(function, start, stop, n, IntegrationMethods.Central)
+    Integration.calculate(function, start, stop, n, Integration.INTEGRATION_METHOD_CENTRAL)
 ))
 print("Метод трапеций: {}".format(
-    Integration.calculate(function, start, stop, n, IntegrationMethods.Trapezoid)
+    Integration.calculate(function, start, stop, n, Integration.INTEGRATION_METHOD_TRAPEZOID)
 ))
 print("Метод парабол: {}".format(
-    Integration.calculate(function, start, stop, n, IntegrationMethods.Parabola)
+    Integration.calculate(function, start, stop, n, Integration.INTEGRATION_METHOD_PARABOLA)
 ))
